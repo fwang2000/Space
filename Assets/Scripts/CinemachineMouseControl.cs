a4662aee1f6f358d6a206c246941adff7c5b9db3
fwang2000 <61210395+fwang2000@users.noreply.github.com>
2021-10-16 21:47:33 -0700

    First Commit

---
+++ Assets/Scripts/CinemachineMouseControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+public class CinemachineMouseControl : MonoBehaviour
+{
+    private bool _freeLookActive;
+
+    // Use this for initialization
+    private void Start()
+    {
+        CinemachineCore.GetInputAxis = GetInputAxis;
+    }
+
+    private void Update()
+    {
+        _freeLookActive = Input.GetMouseButton(0); // 0 = left mouse btn or 1 = right
+    }
+
+    private float GetInputAxis(string axisName)
+    {
+        return !_freeLookActive ? 0 : Input.GetAxis(axisName == "Mouse Y" ? "Mouse Y" : "Mouse X");
+    }
+}
